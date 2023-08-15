a9dfad2efd9b0f0c1cebfc1a014eeab51e18d430
309 <309@academicos.cesde.edu.co>
2023-08-15 08:37:35 -0500

    feat:se reciben datos por teclado y se castea

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,14 +1,22 @@
 #Comentario Linea
 '''Comentario Bloque'''
-NombreUsuario = "Mateo Londoño"
+'''NombreUsuario = "Mateo Londoño"
 EdadUsuario = 20
 EstaturaUsuario = 1.65
 EsHinchaDelVerde = True
 
 print(f"Su Nombre Es: {NombreUsuario} y tiene{EdadUsuario}años y mide{EstaturaUsuario}cm")
-
-
 ComidasFavoritas = ["Frijoles","sushi", "Hamburguesa"]
 print(ComidasFavoritas)
 print(ComidasFavoritas[2])
 
+#Entradas por teclado
+LugarTrabajoUsuario = input("Digita el lugar de trabajo: ")
+print(f"usted trabaja en: {LugarTrabajoUsuario}") '''
+
+#Pedimos Numeros Sumas
+NumeroUno = int(input("Digite el primer numero: "))
+NumeroDos = int(input("Digite el segundo numero: "))
+SumaNumeros = NumeroUno+NumeroDos
+
+print(f"la suma de {NumeroUno}+{NumeroDos}={SumaNumeros}")
